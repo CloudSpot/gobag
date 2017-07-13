5359f648526508edb9bd81de324db63f0f0d3dd8
Danny Krainas <dkrainas87@gmail.com>
2017-07-13 14:37:43 -0400

    change util/token to use ksuid and drop seed string

--- util/token/token.go
+++ util/token/token.go
@@ -1,13 +1,8 @@
 package token
 
-import (
-	"crypto/md5"
-	"fmt"
-	"time"
+import "github.com/segmentio/ksuid"
 
-	"github.com/satori/go.uuid"
-)
-
-func Generate(seed string) string {
-	return fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s.%d.%s", uuid.NewV4().String(), time.Now().Unix(), seed))))
+func Generate() string {
+	uid, _ := ksuid.NewRandom()
+	return uid.String()
 }
