0c8a1ae5169104da1cc4a44b0565cf06e85f7649
Danny Krainas <dkrainas87@gmail.com>
2019-09-04 14:11:07 -0400

    add baghttp helpers RemoteAddr and RemoteIP

--- http/baghttp.go
+++ http/baghttp.go
@@ -1,9 +1,51 @@
 package baghttp
 
 import (
+	"net"
 	"net/http"
+	"strings"
 	"sync/atomic"
+
+	"github.com/prometheus/common/log"
 )
+
+func parseIP(s string) net.IP {
+	ip := net.ParseIP(s)
+	if ip == nil {
+		log.Warnf("invalid remote IP address: %q", s)
+	}
+
+	return ip
+}
+
+func RemoteAddr(r *http.Request) string {
+	if prior := r.Header.Get("X-Forwarded-For"); prior != "" {
+		proxies := strings.Split(prior, ",")
+		if len(proxies) > 0 {
+			remoteAddr := strings.Trim(proxies[0], " ")
+			if parseIP(remoteAddr) != nil {
+				return remoteAddr
+			}
+		}
+	}
+
+	if realIP := r.Header.Get("X-Real-Ip"); realIP != "" {
+		if parseIP(realIP) != nil {
+			return realIP
+		}
+	}
+
+	return r.RemoteAddr
+}
+
+func RemoteIP(r *http.Request) string {
+	addr := RemoteAddr(r)
+	if ip, _, err := net.SplitHostPort(addr); err == nil {
+		return ip
+	}
+
+	return addr
+}
 
 func NewInstrumentedResponseWriter(w http.ResponseWriter) (http.ResponseWriter, *InstrumentedResponseWriter) {
 	if closeNotifier, ok := w.(http.CloseNotifier); ok {
